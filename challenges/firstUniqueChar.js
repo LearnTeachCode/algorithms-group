2b2a610705dbb5388e14ee2570000f3580beb458
Thuy <thuy@thuy.life>
2018-01-12 01:52:48 -0800

    Create firstUniqueChar.js

---
+++ challenges/firstUniqueChar.js
@@ -0,0 +1,11 @@
+/** Find the first unique character in a string. Return `null` if no unique character is found. 
+
+Test cases: 
+  - input: "aabcdcd" , output: "b"
+  - input: "bb", output: null
+  - input: "ababccd", output: "d" 
+  - input: "abcdea", output: "b"
+  
+  Time complexity: 
+  Space complexity:
+**/
